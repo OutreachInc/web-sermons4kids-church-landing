468808cfcf18fcbd4a0a9ba87cf6e28ec646cf44
Chris <cevans@outreach.com>
2020-09-23 11:35:30 -0600

    Styling

--- pages/_church.vue
+++ pages/_church.vue
@@ -1,76 +1,88 @@
 <template>
-<div class="h-screen">
+<div class="h-screen font-body">
   <div class="relative h-80" :style="gradientBackground">
-    <div class="flex justify-between items-center w-full z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h2 class="text-2xl text-white">
-        <NuxtLink :to="slug" class="text-white">{{ church.title }}</NuxtLink>
+    <div class="z-10 flex items-center justify-between w-full px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <h2>
+        <NuxtLink :to="slug" class="font-sans text-3xl font-semibold tracking-tight text-white opacity-75">{{ church.title }}</NuxtLink>
       </h2>
       <Dropdown :label="church.grade.title" :items="dropdownItems" class="mt-4"></Dropdown>
     </div>
-    <div class="absolute bottom-flow-divider bottom-0 h-10 sm:h-14 md:h-16 lg:h-20 xl:h-24 w-full"></div>
+    <div class="absolute bottom-0 w-full h-10 bottom-flow-divider sm:h-14 md:h-16 lg:h-20 xl:h-24"></div>
   </div>
 
-  <div class="flex flex-wrap max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="text-center lg:w-1/2 w-full z-10 -mt-60">
-        <h1 class="text-3xl font-bold text-white">{{ church.sermon.title }}</h1>
-        <div>Lesson for {{date()}}</div>
-        <div>
-          Video Player
-        </div>
-        <div>
-          <div>{{ church.sermon.scripture }}</div>
-          <div></div>
-        </div>
-    </div>
-    <div class="flex items-center justify-center lg:w-1/4 w-full">
-      <div>
-        <h3 class="text-3xl font-medium leading-10 text-curious-blue-500 font-subheading">Activites + Resources</h3>
-        <a href="/" class="inline-flex px-12 py-2 mt-6 text-lg text-white rounded-full bg-minsk-500 hover:bg-minsk-600" download="" target="_blank">
-          Download All
-        </a>
-        <ul class="mt-6">
-          <li class="flex items-center my-4" v-for="resource in church.resources" :key="resource.id">
-            <a
-            :href="resource.s4k_url"
-            target="_blank"
-            class="flex-initial text-xl font-normal leading-6 text-center text-curious-blue-500 font-subheading">
-                <span class="mr-2 text-crusta-500">&rsaquo;</span>
-                {{resource.type}}
-            </a>
-          </li>
-        </ul>
+  <div class="relative z-10 px-4 mx-auto mb-28 max-w-7xl sm:px-6 lg:px-8">
+    <div class="flex flex-wrap -mx-6 space-y-8">
+      <div class="z-10 w-full px-6 text-center lg:w-1/2 -mt-60">
+          <h1 class="text-4xl font-semibold leading-10 tracking-tighter text-white font-subheading">{{ church.sermon.title }}</h1>
+          <div class="mt-4 font-sans text-xl font-bold tracking-tight text-white">Lesson for {{ lessonDate() }}</div>
+          <div class="relative flex justify-center mt-8">
+            <video src="https://sermons4kids.s3.amazonaws.com/13741/Water-From-A-Rock.mp4" controls="controls" preload="none" class="w-full rounded-lg shadow-lg outline-none" style=""></video>
+          </div>
+          <div class="mt-6 font-body">
+            <h3 class="text-2xl font-medium text-curious-blue-500">Theme</h3>
+            <p class="text-xl text-gray-600">{{ church.sermon.theme }}</p>
+          </div>
+          <div class="mt-6 font-body">
+            <h3 class="text-2xl font-medium text-curious-blue-500">Scripture</h3>
+            <p class="text-xl text-gray-600">{{ church.sermon.scripture }}</p>
+          </div>
+          <div class="relative flex justify-center mt-8">
+            <video src="https://sermons4kids.s3.amazonaws.com/13741/Water-From-A-Rock.mp4" controls="controls" preload="none" class="w-full rounded-lg shadow-lg outline-none" style=""></video>
+          </div>
       </div>
-    </div>
-    <div class="flex items-center justify-center lg:w-1/4 lg:order-first w-full">
-      <div class="max-w-sm">
-        <h3>Daily Discussion</h3>
-        <p>Daily Discussion questions correspond with the lesson your kids learned the previous Sunday.</p>
-        <div class="mt-2">
-          <label for="email" class="block text-sm font-medium leading-5 text-gray-700">Email</label>
-          <div class="mt-1 relative rounded-md shadow-sm">
-            <input id="email" type="email" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="you@example.com">
-          </div>
+      <div class="flex items-center justify-center w-full px-6 lg:w-1/4">
+        <div>
+          <h3 class="text-3xl font-medium leading-10 text-curious-blue-500 font-subheading">Activites + Resources</h3>
+          <a href="/" class="inline-flex px-12 py-2 mt-6 text-lg text-white rounded-full bg-minsk-500 hover:bg-minsk-600" download="" target="_blank">
+            Download All
+          </a>
+          <ul class="mt-6">
+            <li class="flex items-center my-4" v-for="resource in church.resources" :key="resource.id">
+              <a
+              :href="resource.s4k_url"
+              target="_blank"
+              class="flex-initial text-xl font-normal leading-6 text-center text-curious-blue-500 font-subheading">
+                  <span class="mr-2 text-crusta-500">&rsaquo;</span>
+                  {{resource.type}}
+              </a>
+            </li>
+          </ul>
         </div>
-        <div class="text-center mt-2">OR</div>
-        <div class="">
-          <label for="phone" class="block text-sm font-medium leading-5 text-gray-700">Phone</label>
-          <div class="mt-1 relative rounded-md shadow-sm">
-            <input id="phone" type="tel" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="888-888-8888">
+      </div>
+      <div class="flex items-center justify-center w-full px-6 lg:w-1/4 lg:order-first">
+        <div class="max-w-sm">
+          <h3 class="text-3xl font-semibold leading-tight text-center text-minsk-500">Daily Discussion</h3>
+          <p class="mt-2 text-sm leading-normal">Daily Discussion questions correspond with the lesson your kids learned the previous Sunday.</p>
+          <div class="mt-2">
+            <label for="email" class="block text-sm font-medium leading-5 text-gray-700">Email</label>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input id="email" type="email" class="block w-full form-input sm:text-sm sm:leading-5" placeholder="you@example.com">
+            </div>
           </div>
-        </div>
-        <div class="flex justify-center">
-        <a href="/" class="inline-flex px-12 py-2 mt-6 text-lg text-white rounded-full bg-minsk-500 hover:bg-minsk-600" target="_blank">
-          Sign Up
-        </a>
+          <div class="mt-2 text-center">OR</div>
+          <div class="">
+            <label for="phone" class="block text-sm font-medium leading-5 text-gray-700">Phone</label>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input id="phone" type="tel" class="block w-full form-input sm:text-sm sm:leading-5" placeholder="888-888-8888">
+            </div>
+          </div>
+          <div class="flex justify-center">
+          <a href="/" class="inline-flex px-12 py-2 mt-6 text-lg text-white rounded-full bg-minsk-500 hover:bg-minsk-600" target="_blank">
+            Sign Up
+          </a>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="sticky bottom-0 h-20 w-full" :style="gradientBackground"></div>
+  <div class="sticky bottom-0 w-full h-20 bg-transparent" :style="gradientBackground">
+    <div class="absolute top-0 w-full h-40 footer-flow-divider"></div>
+  </div>
 </div>
 </template>
 
 <script>
+import { DateTime } from "luxon";
 import Dropdown from "../components/Dropdown.vue";
 export default {
   async asyncData({ params }) {
@@ -126,6 +138,7 @@ export default {
           sermon: {
             id: 9,
             title: "A Bride for Isaac",
+            theme: "Some random theme title",
             s4k_url: "https://web-sermons4kids.test/bride_for_isaac.htm",
             verses: [
               {
@@ -208,8 +221,8 @@ export default {
         },
       };
     },
-    date(){
-      return new Date(this.church.start_time).toLocaleDateString();
+    lessonDate(){
+      return DateTime.fromSQL(this.church.start_time).toLocaleString(DateTime.DATE_FULL);
     },
   },
   computed: {
@@ -232,6 +245,11 @@ export default {
 .bottom-flow-divider{
   background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDBweCIgdmlld0JveD0iMCAwIDEyODAgMTQwIiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxnIGZpbGw9IiNmZmZmZmYiPjxwYXRoIGQ9Ik0xMjgwIDMuNEMxMDUwLjU5IDE4IDEwMTkuNCA4NC44OSA3MzQuNDIgODQuODljLTMyMCAwLTMyMC04NC4zLTY0MC04NC4zQzU5LjQuNTkgMjguMiAxLjYgMCAzLjRWMTQwaDEyODB6IiBmaWxsLW9wYWNpdHk9Ii4zIi8+PHBhdGggZD0iTTAgMjQuMzFjNDMuNDYtNS42OSA5NC41Ni05LjI1IDE1OC40Mi05LjI1IDMyMCAwIDMyMCA4OS4yNCA2NDAgODkuMjQgMjU2LjEzIDAgMzA3LjI4LTU3LjE2IDQ4MS41OC04MFYxNDBIMHoiIGZpbGwtb3BhY2l0eT0iLjUiLz48cGF0aCBkPSJNMTI4MCA1MS43NmMtMjAxIDEyLjQ5LTI0Mi40MyA1My40LTUxMy41OCA1My40LTMyMCAwLTMyMC01Ny02NDAtNTctNDguODUuMDEtOTAuMjEgMS4zNS0xMjYuNDIgMy42VjE0MGgxMjgweiIvPjwvZz48L3N2Zz4=);
     background-size: 100% 100%;
+    z-index: 1;
+}
+.footer-flow-divider{
+  background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxOTFweCIgdmlld0JveD0iMCAwIDEyODAgMTQwIiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxnIGZpbGw9IiNmZmZmZmYiPjxwYXRoIGQ9Ik0wIDB2LjQ4QzE4LjYyIDkuMzggMjk3LjgxIDE0MCA2MzkuNSAxNDAgOTkzLjI0IDE0MCAxMjgwIDAgMTI4MCAweiIgZmlsbC1vcGFjaXR5PSIuMyIvPjxwYXRoIGQ9Ik0wIC42YzE0IDguMjggMTc2LjU0IDk5LjggNTU1LjQ1IDExOS4xNEM5NTIuNDEgMTQwIDEyODAgMCAxMjgwIDBIMHoiIGZpbGwtb3BhY2l0eT0iLjUiLz48cGF0aCBkPSJNNzI2LjI5IDEwMS4yQzExMjYuMzYgNzkuOTIgMTI4MSAwIDEyODEgMEgxYy4wNSAwIDMyNS4yNSAxMjIuNDggNzI1LjI5IDEwMS4yeiIvPjwvZz48L3N2Zz4=);
+  background-size: 100% 100%;
     z-index: 1;
 }
 </style>
