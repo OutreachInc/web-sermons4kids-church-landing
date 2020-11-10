327b2f7fb422f194c65465d19fa06dac892f06b0
Chris <cevans@outreach.com>
2020-11-10 14:28:25 -0700

    Leave room for logo on mobile

--- pages/_church/_class.vue
+++ pages/_church/_class.vue
@@ -12,7 +12,7 @@
 
   <div class="relative z-10 px-4 mx-auto mb-28 max-w-7xl sm:px-6 lg:px-8">
     <div class="flex flex-wrap space-y-8 lg:-mx-6">
-      <div v-if="event" class="z-10 w-full px-6 text-center lg:w-1/2" :class="{'-mt-52': !event.video_embed, '-mt-60': !!event.video_embed}">
+      <div v-if="event" class="z-10 w-full px-6 text-center lg:w-1/2 -mt-52">
           <h1 class="text-4xl font-semibold leading-10 tracking-tighter text-white font-subheading">{{ event.sermon.title }}</h1>
           <div class="mt-4 font-sans text-xl font-bold tracking-tight text-white">{{ translate('Lesson_for') }} {{ lessonDate() }}</div>
 
