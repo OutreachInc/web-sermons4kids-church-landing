83bfcc8b0505dddca3a8fe9288026fab2e30c9ad
Chris <cevans@outreach.com>
2020-11-10 13:42:29 -0700

    Add icon for S4K links

--- pages/_church/index.vue
+++ pages/_church/index.vue
@@ -66,17 +66,18 @@
               :href="resource.media_url"
               target="_blank"
               download
-              class="flex-initial my-4 text-xl font-normal leading-6 text-center text-curious-blue-500 font-subheading">
-                  <span class="mr-2 text-crusta-500">&rsaquo;</span>
+              class="inline-flex my-2 text-xl font-normal leading-6 text-center text-curious-blue-500 font-subheading">
+                  <span class="mr-3 text-crusta-500">&rsaquo;</span>
                   {{resource.type}}
               </a>
               <a
               v-else-if="resource"
               :href="resource.s4k_url"
               target="_blank"
-              class="flex-initial my-4 text-xl font-normal leading-6 text-center text-curious-blue-500 font-subheading">
-                  <span class="mr-2 text-crusta-500">&rsaquo;</span>
+              class="inline-flex my-2 text-xl font-normal leading-6 text-center text-curious-blue-500 font-subheading">
+                  <span class="mr-3 text-crusta-500">&rsaquo;</span>
                   {{resource.type}}
+                  <svg class="w-4 h-4 pb-0.5 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
               </a>
             </li>
           </ul>
